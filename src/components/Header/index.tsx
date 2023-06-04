4fe0267851f82eb4466d6de7c0d3af7e62508477
Marcos Botene <marcosjbotene@gmail.com>
2023-06-04 20:34:51 -0300

    chore: added project logo

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,3 +1,24 @@
-export default function Header() {
-  // TODO
+import Image from 'next/image';
+import Link from 'next/link';
+
+import styles from './header.module.scss';
+
+export function Header() {
+  return (
+    <header className={styles.headerContainer}>
+      <div className={styles.headerContent}>
+        <Link href='/'>
+          <a href='/'>
+            <Image
+              src='/assets/logo.svg'
+              alt='logo'
+              width='238px'
+              height='26px'
+              priority
+            />
+          </a>
+        </Link>
+      </div>
+    </header>
+  );
 }
