7119defe5cd0f14ebc59d9bef1e072b0f7f1e255
Marcos Botene <marcosjbotene@gmail.com>
2023-06-06 22:30:56 -0300

    feat: listing posts in home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,10 @@
 import { GetStaticProps } from 'next';
+import { FiCalendar, FiUser } from 'react-icons/fi';
 
+import { Container } from '../components';
 import { getPrismicClient } from '../services/prismic';
 import commonStyles from '../styles/common.module.scss';
+import { formatPostPublicationDate } from '../utils';
 import styles from './home.module.scss';
 
 interface Post {
@@ -23,17 +26,51 @@ interface HomeProps {
   postsPagination: PostPagination;
 }
 
-export default function Home() {
+const TWENTY_FOUR_HOURS = 60 * 60 * 24;
+
+export default function Home({ postsPagination }: HomeProps) {
   return (
-    <div>
-      <h1>Home</h1>
-    </div>
+    <Container>
+      <div className={styles.homeContent}>
+        {postsPagination.results.map(post => (
+          <div className={styles.postContainer} key={post.uid}>
+            <h2>{post.data.title}</h2>
+            <p>{post.data.subtitle}</p>
+
+            <div className={styles.infoContainer}>
+              <span>
+                <FiCalendar fontSize='1.25rem' /> {post.first_publication_date}
+              </span>
+              <span>
+                <FiUser fontSize='1.25rem' /> {post.data.author}
+              </span>
+            </div>
+          </div>
+        ))}
+      </div>
+    </Container>
   );
 }
 
-// export const getStaticProps = async () => {
-//   // const prismic = getPrismicClient({});
-//   // const postsResponse = await prismic.getByType(TODO);
+export const getStaticProps: GetStaticProps = async () => {
+  const prismic = getPrismicClient({});
 
-//   // TODO
-// };
+  const posts = await prismic.getByType('posts', {
+    pageSize: 5,
+  });
+
+  const formattedPosts = {
+    next_page: posts.next_page,
+    results: posts.results.map(post => ({
+      ...post,
+      first_publication_date: formatPostPublicationDate(
+        post.first_publication_date
+      ),
+    })),
+  };
+
+  return {
+    props: { postsPagination: formattedPosts },
+    revalidate: TWENTY_FOUR_HOURS,
+  };
+};
