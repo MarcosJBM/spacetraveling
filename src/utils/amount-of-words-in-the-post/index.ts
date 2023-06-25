75bf1f2984685018efad3a8a9cf0bcaef7c90d83
Marcos Botene <marcosjbotene@gmail.com>
2023-06-25 20:18:36 -0300

    refactor: counting words present in the heading of each section of the post

--- src/utils/amount-of-words-in-the-post/index.ts
+++ src/utils/amount-of-words-in-the-post/index.ts
@@ -1,15 +1,22 @@
-interface ContentProps {
+type ContentProps = {
+  heading: string;
   body: {
     text: string;
   }[];
-}
+}[];
 
-export function amountOfWordsInThePost(content: ContentProps[]) {
+export function amountOfWordsInThePost(content: ContentProps) {
   const arrayOfWords = content.reduce((accumulator: string[], currentValue) => {
-    const wordsInBody = currentValue.body.map(item => item.text.split(' '));
+    const textInBody = currentValue.body.flatMap(value =>
+      value.text.split(/[^a-zA-Z0-9]+/)
+    );
 
-    return accumulator.concat(...wordsInBody);
+    const textInHeading = currentValue.heading.split(/[^a-zA-Z0-9]+/);
+
+    return accumulator.concat(textInHeading, textInBody);
   }, []);
 
-  return arrayOfWords.length;
+  const arrayOfNonEmptyWords = arrayOfWords.filter(value => value !== '');
+
+  return arrayOfNonEmptyWords.length;
 }
