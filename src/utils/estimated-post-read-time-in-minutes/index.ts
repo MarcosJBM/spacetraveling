e0791995c86e65c59d511a79dd0c4496190965dd
Marcos Botene <marcosjbotene@gmail.com>
2023-07-13 23:23:01 -0300

    fix: using correct math method
    rounding the read time to the nearest integer

--- src/utils/estimated-post-read-time-in-minutes/index.ts
+++ src/utils/estimated-post-read-time-in-minutes/index.ts
@@ -1,5 +1,5 @@
 const AVERAGE_WORDS_READ_PER_MINUTE = 200;
 
 export function estimatedPostReadTimeInMinutes(amountOfWordsInThePost: number) {
-  return Math.round(amountOfWordsInThePost / AVERAGE_WORDS_READ_PER_MINUTE);
+  return Math.ceil(amountOfWordsInThePost / AVERAGE_WORDS_READ_PER_MINUTE);
 }
